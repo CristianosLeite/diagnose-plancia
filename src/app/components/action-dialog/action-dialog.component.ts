bab52464bde7af54279e4bdd72f08b3878c0c249
Cristiano Leite <cristianosleite@hotmail.com>
2024-08-22 18:53:41 -0300

    Started action dialog implementation

---
+++ src/app/components/action-dialog/action-dialog.component.ts
@@ -0,0 +1,34 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatLabel, MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { Activity } from '../../interfaces/activity.interface';
+import { ActivityDialogComponent } from '../activity-dialog/activity-dialog.component';
+
+@Component({
+  selector: 'app-action-dialog',
+  standalone: true,
+  imports: [
+    MatLabel,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule
+  ],
+  templateUrl: './action-dialog.component.html',
+  styleUrl: './action-dialog.component.scss'
+})
+export class ActionDialogComponent {
+  activity = {} as Activity;
+
+  constructor(
+    public dialogRef: MatDialogRef<ActivityDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: Activity) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
--- src/app/components/activity-dialog/activity-dialog.component.ts
+++ src/app/components/activity-dialog/activity-dialog.component.ts
@@ -1,20 +1,22 @@
 import { Component, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
-import { MatFormField, MatLabel, MatFormFieldModule } from '@angular/material/form-field';
-import {MatButtonModule} from '@angular/material/button';
+import { MatLabel, MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleChange, MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
 import { Activity } from '../../interfaces/activity.interface';
-import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MainComponent } from '../main/main.component';
+import { MatDialog } from '@angular/material/dialog';
 import { NgFor } from '@angular/common';
+import { ActionDialogComponent } from '../action-dialog/action-dialog.component';
 
 @Component({
   selector: 'app-activity-dialog',
   standalone: true,
   imports: [
-    MatFormField,
     MatLabel,
     MatDialogModule,
     MatFormFieldModule,
@@ -23,6 +25,7 @@ import { NgFor } from '@angular/common';
     MatButtonModule,
     FormsModule,
     MatCheckboxModule,
+    MatSlideToggleModule,
     NgFor
   ],
   templateUrl: './activity-dialog.component.html',
@@ -32,10 +35,30 @@ export class ActivityDialogComponent {
   activity = {} as Activity;
 
   constructor(
+    public dialog: MatDialog,
     public dialogRef: MatDialogRef<MainComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Activity) {}
+    @Inject(MAT_DIALOG_DATA) public data: Activity
+  ) {}
 
   onNoClick(): void {
     this.dialogRef.close();
+  }
+
+  openDialog(event: MatSlideToggleChange, activity: Activity): boolean {
+    if (event.checked) {
+      activity.status = 'OK';
+      return false;
+    }
+    const dialogRef = this.dialog.open(ActionDialogComponent, {
+      width: '400px',
+      data: activity
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.activity.origin = result;
+    });
+
+    activity.status = 'KO';
+    return false;
   }
 }
