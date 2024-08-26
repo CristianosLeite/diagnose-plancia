706c0005beb6156528b2b5798693f22f53ae76a2
Cristiano Leite <cristianosleite@hotmail.com>
2024-08-26 00:02:23 -0300

    removed unnecessary imports

--- src/app/components/activity-table/activity-table.component.ts
+++ src/app/components/activity-table/activity-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatTableDataSource } from '@angular/material/table';
@@ -6,7 +6,7 @@ import { SelectionModel } from '@angular/cdk/collections';
 import { Activity } from '../../interfaces/activity.interface';
 import { ActivityService } from '../../services/activity.service';
 import { MatIconButton } from '@angular/material/button';
-import { MatMenuModule, MatMenuPanel, MatMenuTrigger } from '@angular/material/menu';
+import { MatMenuModule, MatMenuTrigger } from '@angular/material/menu';
 
 @Component({
   selector: 'app-activity-table',
