8fb5718fa329bcbdbf1d86ad1730b9b113a9c361
Cristiano Leite <cristianosleite@hotmail.com>
2024-08-28 22:48:42 -0300

    Created routes file

--- server.ts
+++ server.ts
@@ -5,8 +5,7 @@ import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 import multer from 'multer';
-import { existsSync } from 'node:fs';
-import fs from 'fs';
+import { fileUpload } from './routes';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -19,6 +18,8 @@ export function app(): express.Express {
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
   });
+
+  server.use(express.urlencoded({ extended: true }));
 
   const serverDistFolder = dirname(fileURLToPath(import.meta.url));
   const browserDistFolder = resolve(serverDistFolder, '../browser');
@@ -54,35 +55,7 @@ export function app(): express.Express {
 
   const upload = multer({ dest: 'uploads/' });
 
-  server.post('/upload', upload.single('file'), (req, res) => {
-    if (!req.file) {
-      return res.status(400).json({ error: 'No files were uploaded.' });
-    }
-
-    const file = req.file;
-    const dir = join(browserDistFolder, 'uploads');
-
-    if (!existsSync(dir)) {
-      fs.mkdirSync(dir, { recursive: true });
-    }
-
-    const targetPath = join(dir, file.originalname);
-
-    fs.rename(file.path, targetPath, (err) => {
-      if (err) {
-        return res.status(500).json({ error: err.message });
-      }
-      res.json({
-        message: 'File uploaded!',
-        fileName: file.originalname,
-        filePath: browserDistFolder + '/uploads/' + file.originalname
-      });
-
-      return;
-    });
-
-    return;
-  });
+  fileUpload(server, upload, browserDistFolder);
 
   return server;
 }
