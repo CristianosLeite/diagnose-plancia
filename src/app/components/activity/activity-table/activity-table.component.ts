13008db8547ce0efdd1bafd78f02af62ed3c9384
Cristiano Leite <cristianosleite@hotmail.com>
2024-09-11 15:23:17 -0300

    Updated activity table context

--- src/app/components/activity/activity-table/activity-table.component.ts
+++ src/app/components/activity/activity-table/activity-table.component.ts
@@ -10,6 +10,8 @@ import { Router, RouterLink, ActivatedRoute } from '@angular/router';
 import { TimeDateService } from '../../../services/time-date/time-date.service';
 import { ChecklistService } from '../../../services/checklist/checklist.service';
 import { AuthService } from '../../../services/auth/auth.service';
+import { MatDialog } from '@angular/material/dialog';
+import { PopupConfirmationComponent } from '../../modal/popup-confirmation/popup-confirmation.component';
 
 @Component({
   selector: 'app-activity-table',
@@ -27,20 +29,20 @@ import { AuthService } from '../../../services/auth/auth.service';
 })
 export class ActivityTableComponent implements OnInit {
   loggedUser = this.auth.loggedUser;
-  dataLength = 0;
   ELEMENT_DATA = this.activityService.ELEMENT_DATA;
-  displayedColumns: string[] = ['id', 'point', 'description', 'sop', 'estimatedTime', 'frequency', 'select', 'options'];
+  displayedColumns: string[] = ['id', 'point', 'description', 'sop', 'estimatedTime', 'frequency'];
   @Input() dataSource = new MatTableDataSource<Partial<Activity>>(this.ELEMENT_DATA);
   @Input() selection = new SelectionModel<Partial<Activity>>(true, []);
   @Input() context = 'activity';
 
   constructor(
+    public timeDateService: TimeDateService,
+    public dialog: MatDialog,
     private activityService: ActivityService,
     private checklistService: ChecklistService,
     private auth: AuthService,
     private router: Router,
     private ActivatedRoute: ActivatedRoute,
-    public timeDateService: TimeDateService,
     private cdr: ChangeDetectorRef
   ) {
     this.activityService.activityCanceled.subscribe((activity: Partial<Activity>) => {
@@ -52,11 +54,11 @@ export class ActivityTableComponent implements OnInit {
     this.activityService.activitiesChanged.subscribe((data: Activity[]) => {
       this.dataSource = new MatTableDataSource<Partial<Activity>>(data);
       this.ELEMENT_DATA = data;
-      this.cdr.detectChanges();
     });
     this.checklistService.checklistChanged.subscribe((data: Activity[]) => {
       this.ELEMENT_DATA = data;
       this.filterActivities(data);
+      this.cdr.detectChanges();
     });
     this.ActivatedRoute.params.subscribe((params) => {
       this.context = params['context'];
@@ -70,8 +72,10 @@ export class ActivityTableComponent implements OnInit {
 
   private fetchDataSource() {
     if (this.context === 'activity') {
+      this.displayedColumns = ['id', 'point', 'description', 'sop', 'options'];
       this.activityService.retrieveAllActivities();
     } else if (this.context === 'checklist') {
+      this.displayedColumns = ['id', 'point', 'description', 'sop', 'estimatedTime', 'frequency', 'select'];
       this.checklistService.retrievePendingChecklist(this.loggedUser.shift_work);
     }
   }
@@ -96,7 +100,6 @@ export class ActivityTableComponent implements OnInit {
           return false;
       }
     });
-    this.dataLength = this.ELEMENT_DATA.length;
     this.dataSource = new MatTableDataSource<Partial<Activity>>(this.ELEMENT_DATA);
     this.cdr.detectChanges();
   }
@@ -142,9 +145,34 @@ export class ActivityTableComponent implements OnInit {
     ]);
   }
 
-  deleteActivity(event: any, row: Activity) {
+  private deleteActivity(activity: Activity) {
+    this.activityService.deleteActivity(activity.activity_id.toString()).subscribe(() => {
+      this.activityService.retrieveAllActivities();
+    });
+  }
+
+  openPopupConfirmation(event: any, activity: Activity): boolean {
     event.stopPropagation();
-    alert('Delete');
+    const dialogRef = this.dialog.open(PopupConfirmationComponent, {
+      width: '400px',
+      data: {
+        activity,
+        title: 'Confirmar exclusão',
+        message: 'Deseja excluir a atividade?'
+      }
+    });
+
+    const onCancelSubscription = dialogRef.componentInstance.onCancel.subscribe(() => {
+      dialogRef.close();
+      onCancelSubscription.unsubscribe();
+    });
+
+    const onConfirmSubscription = dialogRef.componentInstance.onConfirm.subscribe(() => {
+      this.deleteActivity(activity);
+      onConfirmSubscription.unsubscribe();
+    });
+
+    return false;
   }
 
   handleFrequency(frequency: string): string {
