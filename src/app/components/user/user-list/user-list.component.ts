6a769bbb626237a620bbe76aec63e9f4b018f8d5
Kassielm <kassielsmoreira@gmail.com>
2024-09-12 13:03:22 -0300

    Add delete option.

--- src/app/components/user/user-list/user-list.component.ts
+++ src/app/components/user/user-list/user-list.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { UserService } from '../../../services/user/user.service';
 import { User, Permissions } from '../../../interfaces/user.interface';
-import { MatSnackBar } from '@angular/material/snack-bar';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
@@ -11,6 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatMenuModule } from '@angular/material/menu';
 import { CaptionModule } from '../../../directives/caption/caption.module';
+import { SnackbarService } from './../../../services/snack-bar/snack-bar.service';
 import { NgFor, NgIf, NgStyle } from '@angular/common';
 import { TooltipModule } from '../../tooltip/tooltip.module';
 
@@ -29,16 +29,29 @@ import { TooltipModule } from '../../tooltip/tooltip.module';
     MatButtonModule,
     MatIconModule,
     MatSnackBarModule,
-    MatMenuModule
+    MatMenuModule,
   ],
   templateUrl: './user-list.component.html',
-  styleUrls: ['./user-list.component.scss']
+  styleUrls: ['./user-list.component.scss'],
 })
 export class UserListComponent implements OnInit {
-  displayedColumns: string[] = ['badge_number', 'name', 'origin', 'company', 'plant', 'permissions', 'options'];
+  displayedColumns: string[] = [
+    'badge_number',
+    'name',
+    'origin',
+    'company',
+    'plant',
+    'permissions',
+    'options',
+  ];
   dataSource = new MatTableDataSource<User>();
+  showModal = false;
+  selectedElement: any = null;
 
-  constructor(private userService: UserService, private snackBar: MatSnackBar) { }
+  constructor(
+    private userService: UserService,
+    private snackBar: SnackbarService
+  ) {}
 
   ngOnInit() {
     this.userService.retrieveAllUsers().subscribe((users: User[]) => {
@@ -46,26 +59,38 @@ export class UserListComponent implements OnInit {
     });
   }
 
+  openModalConfirmation(event: Event, element: any) {
+    event.stopPropagation();
+    this.showModal = true;
+    this.selectedElement = element.user_id;
+  }
+
+  closeModal() {
+    this.showModal = false;
+  }
+
+  confirmDeletion(): void {
+    this.deleteUser(this.selectedElement);
+    this.closeModal();
+  }
+
   deleteUser(userId: string) {
-    if (confirm('Tem certeza que deseja excluir este usuário?')) {
-      this.userService.deleteUser(userId).subscribe(() => {
-        this.snackBar.open('Usuário excluído com sucesso!', 'Fechar', { duration: 3000 });
-        this.userService.retrieveAllUsers().subscribe((users: User[]) => {
-          this.dataSource.data = users;
-        });
+    this.userService.deleteUser(userId).subscribe(() => {
+      this.snackBar.openSnackBar('user_delete_success');
+      this.userService.retrieveAllUsers().subscribe((users: User[]) => {
+        this.dataSource.data = users;
       });
-    }
+    });
   }
 
   getALLPermissions() {
     if (!this.dataSource.data) return [];
     let permissions: Permissions[] = [];
-    this.dataSource.data.forEach(user => {
+    this.dataSource.data.forEach((user) => {
       if (user.permissions) {
         permissions = [
           ...permissions,
-          ...user.permissions
-            .map(permission => permission as Permissions)
+          ...user.permissions.map((permission) => permission as Permissions),
         ];
       }
     });
@@ -74,14 +99,14 @@ export class UserListComponent implements OnInit {
 
   handlePermissions(permission: string) {
     const value: { [key: string]: string } = {
-      'create_users': 'Cadastrar usuários',
-      'view_users': 'Visualizar usuários cadastrados',
-      'edit_users': 'Editar usuários',
-      'view_history': 'Visualizar histórico de atividades',
-      'create_checklist': 'Realizar checklist',
-      'create_activity': 'Cadastrar nova atividade',
-      'view_activity': 'Visualizar atividades cadastradas',
-      'reports': 'Exportar Relatórios'
+      create_users: 'Cadastrar usuários',
+      view_users: 'Visualizar usuários cadastrados',
+      edit_users: 'Editar usuários',
+      view_history: 'Visualizar histórico de atividades',
+      create_checklist: 'Realizar checklist',
+      create_activity: 'Cadastrar nova atividade',
+      view_activity: 'Visualizar atividades cadastradas',
+      reports: 'Exportar Relatórios',
     };
 
     return value[permission];
